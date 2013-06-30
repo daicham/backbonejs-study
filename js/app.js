b4f65d657bc982b35480a26f55e0d023d0e91834
daicham <daicham@gmail.com>
2013-06-30 12:58:15 +0900

    Usage of accessors for attributes and define a method of Model

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Task = Backbone.Model.extend({
   defaults: {
     title: "do something",
     completed: false
+  },
+  toggle: function() {
+    this.set('completed', !this.get('completed'));
   }
 });
 
@@ -13,6 +16,12 @@ var task1 = new Task({
   completed: true
 });
 
+task1.set('title', 'newTitle');
+var title = task1.get('title')
+
+console.log(task1.toJSON());
+console.log(title);
+task1.toggle();
 console.log(task1.toJSON());
 
 })();
