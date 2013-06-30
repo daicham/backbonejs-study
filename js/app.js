e2f1bfcc5b4609ea7d9338c0a4acd0b754df59e2
daicham <daicham@gmail.com>
2013-06-30 13:11:38 +0900

    Clean app.js for studying about View

--- js/app.js
+++ js/app.js
@@ -7,29 +7,7 @@ var Task = Backbone.Model.extend({
     title: "do something",
     completed: false
   },
-  validate: function(attrs) {
-    if (_.isEmpty(attrs.title)) {
-      return "title must not be empty!";
-    }
-  },
-  toggle: function() {
-    this.set('completed', !this.get('completed'));
-  }
 });
-
-var task1 = new Task({
-  completed: true
-});
-
-task1.set('title', 'newTitle');
-var title = task1.get('title')
-
-console.log(task1.toJSON());
-console.log(title);
-task1.toggle();
-console.log(task1.toJSON());
-
-task1.set({title: ''}, {validate: true});
-console.log(task1.toJSON()); // title is not changed.
+var task = new Task()
 
 })();
