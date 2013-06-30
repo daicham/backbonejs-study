09f0bb97a7b93c54f2d035b761e8c52203f51c3c
daicham <daicham@gmail.com>
2013-06-30 14:47:26 +0900

    Add delete button
    - Add event for delete
    - Add event listener of destroy event
    - Add remove function for removeing dom

--- js/app.js
+++ js/app.js
@@ -10,6 +10,20 @@ var Tasks = Backbone.Collection.extend({ model: Task });
 
 var TaskView = Backbone.View.extend({
   tagName: 'li',
+  initialize: function() {
+    this.model.on('destroy', this.remove, this);
+  },
+  events: {
+    'click .delete': 'destroy'
+  },
+  destroy: function () {
+    if (confirm('are you sure?')) {
+      this.model.destroy();
+    }
+  },
+  remove: function() {
+    this.$el.remove();
+  },
   template: _.template($('#task-template').html()),
   render: function() {
     var template = this.template(this.model.toJSON());
@@ -17,7 +31,16 @@ var TaskView = Backbone.View.extend({
     return this;
   }
 });
-var TasksView = Backbone.View.extend({});
+var TasksView = Backbone.View.extend({
+  tagName: 'ul',
+  render: function() {
+    this.collection.each(function (task) {
+      var taskView = new TaskView({model: task});
+      this.$el.append(taskView.render().el);
+    }, this);
+    return this;
+  }
+});
 
 var tasks = new Tasks([
   {
@@ -31,4 +54,8 @@ var tasks = new Tasks([
     title: 'task3'
   }
 ]);
+
+var tasksView = new TasksView({collection: tasks});
+$('#tasks').html(tasksView.render().el);
+
 })();
