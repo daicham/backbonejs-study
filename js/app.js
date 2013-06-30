d08fdf66824da7abd08b91bed9fd5003ee1ee4a3
daicham <daicham@gmail.com>
2013-06-30 13:40:22 +0900

    Define a view template
    - We can define some attributes. (class, id)
    - Using _#template() provided by underscore.js. "<%- ... %>" works for HTML-escaped.
    - Define a render method.
    - Call the template method with model's JSON object to get a label which should be shown.

--- js/app.js
+++ js/app.js
@@ -11,11 +11,20 @@ var Task = Backbone.Model.extend({
 var task = new Task()
 
 // View
-var TaskView = Backbone.View.extend({
-  tagName: 'li'
-});
-var taskView = new TaskView({ model: task});
 
-console.log(taskView.el);
+var TaskView = Backbone.View.extend({
+  tagName: 'li',
+  className: 'liClass',
+  id: 'liId',
+  template: _.template("<%- title %>"),
+  render: function() {
+    var template = this.template(this.model.toJSON());
+    this.$el.html(template);
+    return this;
+  }
+});
+var taskView = new TaskView({ model: task });
+
+console.log(taskView.render().el);
 
 })();
